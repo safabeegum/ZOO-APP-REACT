828b5c97cc129c2c588d198c45642efd080f1e80
safabeegum <pasafabeegum@gmail.com>
2024-06-07 20:24:54 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddAnimals from './components/AddAnimals';
 import SearchAnimals from './components/SearchAnimals';
+import DeleteAnimals from './components/DeleteAnimals';
 
 function App() {
   return (
     <div>
       <AddAnimals/>
       <SearchAnimals/>
+      <DeleteAnimals/>
     </div>
   );
 }
