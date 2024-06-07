71b95a6c43988b4d5cb10a4f29fb34687834b768
safabeegum <pasafabeegum@gmail.com>
2024-06-07 22:32:29 +0530

    Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">ZOO</a>
+    <Link class="navbar-brand" to="#">ZOO</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">ADD ANIMALS</a>
+          <Link class="nav-link" to="/">ADD ANIMALS</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">SEARCH ANIMALS</a>
+          <Link class="nav-link" to="/search">SEARCH ANIMALS</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">DELETE ANIMALS</a>
+          <Link class="nav-link" to="/delete">DELETE ANIMALS</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">VIEW ANIMALS</a>
+          <Link class="nav-link" to="/viewall">VIEW ANIMALS</Link>
         </li>
       </ul>
     </div>
