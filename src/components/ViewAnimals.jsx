20288df70ebb4674a19f7e48acfb69df92126bf5
safabeegum <pasafabeegum@gmail.com>
2024-06-07 21:32:24 +0530

    Completed

---
+++ src/components/ViewAnimals.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const ViewAnimals = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/majestic-bengal-tiger-fierce-wild-hides-tropical-rainforest-generated-by-artificial-intellingence_25030-63904.jpg?t=st=1717775709~exp=1717779309~hmac=9738aead725d2c5c6193bc380c5258d1db5ab4a031dbbef0d92e824d70382f2b&w=1380" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/beautiful-baby-zebra-sitting-ground-captured-african-jungle_181624-35400.jpg?t=st=1717775773~exp=1717779373~hmac=4bf2c4ed4017a996c4423297325cd992489fb89cd2afa5d645154bcc634cf860&w=1060" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/elephants-national-park-sri-lanka_167946-150.jpg?t=st=1717775848~exp=1717779448~hmac=57ee55f26b156d6bfe49c2565f8874a794d052f687791129bb2ce4caf022a2f4&w=1060" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/african-lion-portrait-warm-light_475641-46.jpg?t=st=1717775878~exp=1717779478~hmac=c26c6464448fbbbfe052170845f2e5f0d97e6eee31097f3ab554b077e12a2191&w=1060" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/closeup-shot-beautiful-peacock-park-daytime_181624-45502.jpg?t=st=1717775945~exp=1717779545~hmac=a4231889c842732915578a26bbf3449680e1fdd23e81c1f9e20c744cd462309e&w=1060" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="card">
+  <img src="https://img.freepik.com/free-photo/giraffes-herd-savannah_155003-6497.jpg?t=st=1717776013~exp=1717779613~hmac=45f21ca17d3dd7fa5279698e65b725b6fb65d4b5538a1e2bbda89494e432cac6&w=1060" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAnimals
